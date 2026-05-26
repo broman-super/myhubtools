8bce94a8929f5a33b82a1b4bdf3d7f904cad7a2a
broman-super <broman.supersub@gmail.com>
2026-05-26 17:11:24 +0700

    Update GAS_WEB_APP_URL in config.js

--- Productive/tr/assets/config.js
+++ Productive/tr/assets/config.js
@@ -7,7 +7,7 @@
 
   /** URL Web App Google Apps Script (deploy terbaru) */
   var GAS_WEB_APP_URL =
-    'https://script.google.com/macros/s/AKfycbwlfk4f6Ygv7IvwEN7G5B9jp7YCR7bEJ18ZbqzmZjFg8wlP-kLj3P5-f4SImcZvXgb09A/exec';
+    'https://script.google.com/macros/s/AKfycbwNPC8SwiV_K1kAca9R5CCfTeQr8RlMNQn6elLoRQfC11xIkYHiPE_BaUtFYSO5VzIVvQ/exec';
 
   global.GAS_URL = GAS_WEB_APP_URL;
 
