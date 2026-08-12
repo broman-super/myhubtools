aab556464af3a8f3a302484f7ac8a472422e72af
broman-super <broman.supersub@gmail.com>
2026-08-12 18:17:16 +0700

    new Update

--- Productive/Project_develop/vite.config.js
+++ Productive/Project_develop/vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteSingleFile } from 'vite-plugin-singlefile';
 
-// base './' supaya asset (JS/CSS) ikut relatif terhadap index.html
-// sehingga hasil build di dist/ bisa di-embed via iframe dari mana saja.
+// base './' supaya asset relatif.
+// viteSingleFile: JS+CSS di-inline ke 1 file index.html mandiri,
+// sehingga hasil build bisa di-double-click (file://) seperti tool lain,
+// sekaligus tetap jalan bila di-serve via http/https.
 export default defineConfig({
   base: './',
-  plugins: [react()],
-  build: { outDir: 'dist' }
+  plugins: [react(), viteSingleFile()],
+  build: {
+    outDir: 'dist',
+    assetsInlineLimit: 100000000,
+    chunkSizeWarningLimit: 100000000
+  }
 });
