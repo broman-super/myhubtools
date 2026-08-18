793f5a95bfdb4a8e1d0c5b4a4668f2bdcb2b4921
broman-super <broman.supersub@gmail.com>
2026-08-18 15:18:26 +0700

    Phase 12 (ringan): font via Google Fonts, buang inline tools.css

--- Productive/Project_develop/src/main.jsx
+++ Productive/Project_develop/src/main.jsx
@@ -3,6 +3,5 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './styles.css';
 import '../../../src/styles/design-system.css';
-import '../../../src/styles/tools.css';
 
 createRoot(document.getElementById('root')).render(<App />);
